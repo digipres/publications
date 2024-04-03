a1f3fa42704b44f2f6a09e7ba788c49ebd9bd38a
Andrew Jackson <andrew.jackson@dpconline.org>
2024-04-03 15:41:13 +0100

    Add other useful fields to the search index, for digipres/digipres-practice-index#4

--- _includes/lunr/custom-index.js
+++ _includes/lunr/custom-index.js
@@ -1,3 +1,3 @@
-const content_to_merge = [docs[i].content, docs[i].abstract];
+const content_to_merge = [docs[i].content, docs[i].abstract, docs[i].creators, docs[i].keywords, docs[i].institutions];
 docs[i].content = content_to_merge.join(' ');
 
